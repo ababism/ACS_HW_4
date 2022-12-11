aec0add860708b029026e0501fe87d2a8db27a11
Ivan Kiselev <kiselev.contact@gmail.com>
2022-12-12 00:27:23 +0300

    working version 1.0

--- main.cpp
+++ main.cpp
@@ -2,68 +2,77 @@
 #include <string>
 #include <pthread.h>
 
-char *p_string;
+//char *p_string;
 size_t string_size;
+char *end_of_string;
 size_t step;
 char *start_it;
 char *end_it;
 
-std::pair<char*, char*> updateCurrentIterators() {
-    if (start_it >= p_string + string_size) {
-        return {nullptr, nullptr}
-    }
-    if (end_it >= p_string + string_size) {
-        return {}
-    }
-    start_it += step;
-    return { }
-}
+pthread_mutex_t mutex;// мьютекс для условных переменных
 
-void* func(void *param) {
-    int p=*(int *)param ;  //номер строки, заполняемой потоком
-    //вычисление элементов результирующей матрицы, стоящих в строке с номером p
-    for (int i=0 ; i<3 ; i++) {
-        C[p][i]=0 ;
-        for (int j=0 ; j<3 ; j++) {
-            C[p][i]+=A[p][j]*B[j][i] ;
+void *func(void *param) {
+    std::cout << "start\n";
+    int p = *(int *) param;  //номер потока
+    char *left_it;
+    char *right_it;
+    do {
+        pthread_mutex_lock(&mutex); //протокол входа в КС: закрыть двоичный семафор
+        //начало критической секции – обращению в портфелю задач
+        left_it = start_it;
+        right_it = end_it;
+        // обновляем значения глобальных переменных портфеля задач
+        start_it = end_it;
+        if (end_it + step >= end_of_string) {
+            end_it = end_of_string;
+        } else {
+            end_it = end_it + step;
         }
-        pthread_mutex_lock(&mutex) ; //протокол входа в КС: закрыть двоичный семафор
-        //начало критической секции – запись результата в очередь
-        newrez = new result ;
-        sprintf(newrez->str,
-                "Поток %d: вычислен элемент [%d][%d] = %d",
-                p, p, i, C[p][i]
-        ) ;
-        newrez->next = NULL ;
-        rez->next = newrez ;
-        rez = newrez ;
-
+        std::cout << param << ": " << *left_it << " and " << *right_it << '\n';
         //конец критической секции
-        pthread_mutex_unlock(&mutex) ;  //протокол выхода из КС:
+        pthread_mutex_unlock(&mutex);  //протокол выхода из КС:
         //открыть двоичный семафор
-        //sleep(1);
-    }
+        for (char* it = left_it; it < right_it; ++it) {
+            *it = (*it - 3);
+        }
+    } while (end_it < end_of_string);
     return nullptr;
 }
+
 int main() {
-    pthread_mutex_t mutex;// мьютекс для условных переменных
     const std::string encrypted("Wklv#wh{w#vkrxog#eh#hqfu|swhg");
     std::string text = encrypted;
-    char key = 3;
-    for (auto i = 0; i < text.size(); ++i) {
-        text[i] = encrypted[i] - 3;
-    }
-    step = text.size();
 
-    pthread_t pthread_first,  pthread_second ;
-    pthread_mutex_init(&mutex, NULL) ; //инициализация двоичного семафора
-//    pthread_mutex_lock(&mutex);
+    start_it = &text[0];
+    string_size = text.size();
+
+    end_of_string = start_it + string_size;
+
+    step = string_size / 3;
+    end_it = start_it;
+    if (end_it + step >= end_of_string) {
+        end_it = end_of_string;
+    } else {
+        end_it = end_it + step;
+    }
+
+    pthread_t pthread_first, pthread_second;
+    pthread_mutex_init(&mutex, NULL); //инициализация двоичного семафора
+//    char key = 3;
+//    for (auto i = 0; i < text.size(); ++i) {
+//        text[i] = encrypted[i] - 3;
+//    }
+//    step = text.size();
+    int num[2];
+    num[0]= 1;
+    num[1] = 2;
+    pthread_create(&pthread_first, NULL, func, (void *)(num)) ;
+    pthread_create(&pthread_second, NULL, func, (void *)(num+1)) ;
+
+    pthread_join(pthread_first, nullptr);
+    pthread_join(pthread_second, nullptr);
 
     std::cout << text << std::endl;
 
     return 0;
 }
-
-
-
-
